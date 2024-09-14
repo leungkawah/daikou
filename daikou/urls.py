d7733c3f5c520c6a20f02a7a4d774761cfb64bb8
Kelvin Leung <kahwaleung@gmail.com>
2024-09-14 11:38:48 +0900

    add redirect funtion

--- daikou/urls.py
+++ daikou/urls.py
@@ -1,19 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
 
-# Use include() to add URLS from the catalog application and authentication system
-from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
-
-urlpatterns += [
     path("shop/", include("shop.urls")),
 ]
 
+urlpatterns += [
+    path("", lambda request: redirect("shop/", permanent=True)),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
